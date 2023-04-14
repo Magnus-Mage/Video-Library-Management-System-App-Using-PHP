1bda8e7e26132319d191e7b94e697245b49e6d0a
Magnus-Mage <104272224+Magnus-Mage@users.noreply.github.com>
2023-04-14 22:55:58 +0530

    3rd commit

--- VLMSApp/VLMSApp/Form1.cs
+++ VLMSApp/VLMSApp/Form1.cs
@@ -12,41 +12,10 @@ namespace VLMSApp
         private void button1_Click(object sender, EventArgs e)
         {
             VideoDAO videoDAO = new VideoDAO();
-            Video_data v1 = new Video_data
-            {
-                video_id = 1,
-                title = "Avengers",
-                director = "Russo Brothers",
-                release_year = 2019,
-                genre = "Action",
-                summary = "Blah blah blah",
-                actors = "Robert Downey Jr.",
-                upload_datetime = new DateTime(1974, 7, 10, 7, 10, 24),
-                duration_minutes = 189,
-                cost_dollars = 14.99,
-                image_name = "Blah_blah_blah"
-            };
-            Video_data v2 = new Video_data
-            {
-                video_id = 2,
-                title = "Revenger",
-                director = "Russo Brothers",
-                release_year = 2019,
-                genre = "Action",
-                summary = "Blah blah blah",
-                actors = "Robert Downey Jr.",
-                upload_datetime = new DateTime(1975, 7, 10, 7, 10, 24),
-                duration_minutes = 149,
-                cost_dollars = 15.99,
-                image_name = "Blah_blah_blah"
-            };
-
-            videoDAO.video_data.Add(v1);
-            videoDAO.video_data.Add(v2);
 
             //connect the list to the grid view control
 
-            videoBindingSource.DataSource = videoDAO.video_data; 
+            videoBindingSource.DataSource = videoDAO.getAllVideo_data(); 
             
             dataGridView1.DataSource = videoBindingSource;
         }
--- VLMSApp/VLMSApp/VideoDAO.cs
+++ VLMSApp/VLMSApp/VideoDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,46 @@ namespace VLMSApp
     internal class VideoDAO
     {
         //VERSION 1 only contains dummy data. No relation to db yet.
-        public List<Video_data> video_data = new List<Video_data>();
+        //public List<Video_data> video_data = new List<Video_data>();
+        string connectionString = "datasource=localhost;port=3306;username=root;password=root;database=vlms;";
+
+        public List<Video_data> getAllVideo_data()
+        {
+            List<Video_data> returnThese = new List<Video_data>();
+
+            //sql connection
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            //define sql statement to fetch all video_data
+            MySqlCommand command = new MySqlCommand("SELECT * FROM video_data", connection);
+
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Video_data v = new Video_data
+                    {
+                        video_id = reader.GetInt32(0),
+                        title = reader.GetString(1),
+                        director = reader.GetString(2),
+                        release_year = reader.GetInt32(3),
+                        genre = reader.GetString(4),
+                        summary = reader.GetString(5),
+                        actors = reader.GetString(6),
+                        upload_datetime = reader.GetDateTime(7),
+                        duration_minutes = reader.GetInt32(8),
+                        cost_dollars = reader.GetInt32(9),
+                        image_name = reader.GetString(10),
+
+                    }
+                }
+            }
+
+
+
+                return returnThese;
+        }
     }
 }
